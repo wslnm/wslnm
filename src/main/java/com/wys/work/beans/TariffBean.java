986dee963d8a469da14f251f620f577e9baacd56
18982 <18982@DESKTOP-AVQ7TL7>
2018-06-14 18:22:14 +0800

    修改Bean

--- src/main/java/com/wys/work/beans/TariffBean.java
+++ src/main/java/com/wys/work/beans/TariffBean.java
@@ -1,14 +1,22 @@
 package com.wys.work.beans;
 
-
-
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * 资费类
  * @author liliuhong
  *
  */
+@Entity
+@Table(name="t_tariff")
 public class TariffBean implements Serializable {
 
 	/**
@@ -16,20 +24,31 @@ public class TariffBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 8182717495869850432L;
 	
+	@Id
+	@Column
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
 	private long id;
 	
+	@Column(name="tariff_name",length=20)
 	private String tariffName;//资费名
 	
+	@Column(name="tariff_type",length=11)
 	private int tariffType;//资费类型
 	
+	@Column(name="basic_time",length=11)
 	private int basicTime;//基本时长
 	
+	@Column(name="basic_cost",length=11)
 	private int basicCost;//基本费用
 	
+	@Column(name="unit_cost",length=11)
 	private int unitCost;//单位费用
 	
+	@Column(name="tariff_directions",length=20)
 	private String tariffDirections;//资费说明
 	
+	@Column(name="tariff_status",length=11)
 	private int tariffStatus;//资费状态
 	
 	public TariffBean() {
