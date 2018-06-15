7bc4580637e3c7778e8b4ee51b3e6341649b4c84
Administrator <Administrator@QH-20151020DXHP>
2018-06-15 16:40:39 +0800

    12

--- src/main/java/com/wys/work/adminmag/mapper/AdminMapper.java
+++ src/main/java/com/wys/work/adminmag/mapper/AdminMapper.java
@@ -3,12 +3,17 @@ package com.wys.work.adminmag.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
 
 import com.wys.work.beans.AdminBean;
 import com.wys.work.beans.Pager;
+import com.wys.work.beans.RoleBean;
 
 /**
  * @author Administrator
@@ -17,6 +22,31 @@ import com.wys.work.beans.Pager;
  */
 public interface AdminMapper {
 
+	/**
+	 * 根据ID查询Admin
+	 * @param id
+	 * @return
+	 */
+	@Results({
+		@Result(id=true,property="id",column="id",javaType=Long.class),
+		@Result(property="adminName",column="admin_name",javaType=String.class),
+		@Result(property="adminAcc",column="admin_acc",javaType=String.class),
+		@Result(property="adminPwd",column="admin_pwd",javaType=String.class),
+		@Result(property="adminTel",column="admin_tel",javaType=String.class),
+		@Result(property="adminEmail",column="admin_email",javaType=String.class),
+		@Result(property="role",javaType=RoleBean.class,column="fk_role_id",one=@One(select="findRoleByID"))
+	})
+	@Select("SELECT * FROM t_admin where id=#{id} ")
+	public AdminBean findAdminById(Long id);
+
+	@Results({
+		@Result(id=true,property="id",column="id",javaType=Long.class),
+		@Result(property="roleName",column="role_name",javaType=String.class),
+		@Result(property="roleType",column="role_type",javaType=String.class)
+	})
+	@Select("select * from t_role where id=#{id}")
+	public RoleBean findRoleByID(Long id);
+	
 	/**
 	 * 查询出的总条数
 	 * @param parmas
