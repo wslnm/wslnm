016289c04996ede7a4470a76ea538daf06a7bb5d
Administrator <Administrator@W8-201705251115>
2018-06-15 15:22:14 +0800

    修改1.1

--- src/main/java/com/wys/work/loginlogmag/handleservice/impl/LoginLogHandleServiceImpl.java
+++ src/main/java/com/wys/work/loginlogmag/handleservice/impl/LoginLogHandleServiceImpl.java
@@ -1,10 +1,16 @@
 package com.wys.work.loginlogmag.handleservice.impl;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
 import com.wys.work.beans.LoginLogBean;
 import com.wys.work.loginlogmag.handlerepository.ILoginLogHandleRepository;
 import com.wys.work.loginlogmag.handleservice.ILoginLogHandleService;
-
+@Service
 public class LoginLogHandleServiceImpl implements ILoginLogHandleService {
+	
+	@Resource
 	public ILoginLogHandleRepository iLoginLogHandleRepository;
 	@Override
 	public void saveLoginLog(LoginLogBean loginlog) {
