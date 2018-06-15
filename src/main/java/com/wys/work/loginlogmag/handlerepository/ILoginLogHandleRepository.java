22402861f7adb2478160a268649fed864b6375e3
bafenbao <18982459119@163.com>
2018-06-15 16:05:30 +0800

    实现

--- src/main/java/com/wys/work/loginlogmag/handlerepository/ILoginLogHandleRepository.java
+++ src/main/java/com/wys/work/loginlogmag/handlerepository/ILoginLogHandleRepository.java
@@ -1,14 +1,17 @@
 package com.wys.work.loginlogmag.handlerepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import com.wys.work.beans.LoginLogBean;
 
-public interface ILoginLogHandleRepository {
+public interface ILoginLogHandleRepository extends JpaRepository<LoginLogBean, Long>,JpaSpecificationExecutor<LoginLogBean>{
 	/**
 	 * 登录成功时，将存储日志信息
 	 * /退出时，存储日志信息
 	 * @param loginlog
 	 * @return
 	 */
-	public void saveLoginLog(LoginLogBean loginlog);
+	
 
 }
