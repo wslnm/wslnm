a191fb6a40b880e9d0ae99eda6dd571394cd950c
Administrator <Administrator@QH-20151020DXHP>
2018-06-20 15:45:20 +0800

    12

--- src/main/java/com/wys/work/businessmag/handlerepository/IBusinessHandleRepository.java
+++ src/main/java/com/wys/work/businessmag/handlerepository/IBusinessHandleRepository.java
@@ -1,31 +1,18 @@
 package com.wys.work.businessmag.handlerepository;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import com.wys.work.beans.BusinessBean;
-import com.wys.work.beans.AdminBean;
 
 /**
- * @author Administrator
+ * @author yangyong
  * @version 1.0
  * @created 14-����-2018 13:07:13
  */
-public interface IBusinessHandleRepository {
+public interface IBusinessHandleRepository extends JpaRepository<BusinessBean, Long>,JpaSpecificationExecutor<BusinessBean>{
 
-	/**
-	 * 
-	 * @param business
-	 */
-	public void delete(BusinessBean business);
-
-	/**
-	 * 
-	 * @param business
-	 */
-	public void saveBusiness(BusinessBean business);
-
-	/**
-	 * 
-	 * @param business
-	 */
-	public void updateBusiness(BusinessBean business);
+	
 
 }
