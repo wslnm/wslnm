a5beb2b1a9680a5f98a9b4a2a429a65f07ad8cb8
Administrator <Administrator@USER-20170201WJ>
2018-06-20 16:28:39 +0800

    1

--- src/main/java/com/wys/work/usermag/controller/UserController.java
+++ src/main/java/com/wys/work/usermag/controller/UserController.java
@@ -1,6 +1,14 @@
 package com.wys.work.usermag.controller;
 
 import com.wys.work.usermag.queryservice.IUserQueryService;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.wys.work.beans.UserBean;
 import com.wys.work.usermag.handleservice.IUserHandleService;
 
 /**
@@ -8,17 +16,35 @@ import com.wys.work.usermag.handleservice.IUserHandleService;
  * @version 1.0
  * @created 14-����-2018 13:07:17
  */
+@RequestMapping("/user")
+@Controller
 public class UserController {
 
-	public IUserQueryService m_IUserQueryService;
-	public IUserHandleService m_IUserHandleService;
-
-	public UserController(){
-
+	
+	@Resource
+	public IUserQueryService userQueryServiceImpl;
+	public IUserHandleService userHandleServiceImpl;
+	
+	
+	@RequestMapping(value="/{id}",method= {RequestMethod.PUT})
+	public void updateUserBean(UserBean userBean) {
+		try {
+			userHandleServiceImpl.updateUserBean(userBean);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return;
 	}
-
-	public void finalize() throws Throwable {
-
+	
+	@RequestMapping(value="/{id}",method= {RequestMethod.DELETE})
+	public void deleteUserBean(UserBean user) {
+		try {
+			userHandleServiceImpl.deleteUserBean(user);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return;
 	}
+	
 
 }
