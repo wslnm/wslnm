ff15ce3cf749f1ab623da9e6fa5b703a9affab13
Administrator <Administrator@QH-20151020DXHP>
2018-06-24 15:10:18 +0800

    添加adminmag中controller内容

--- src/main/java/com/wys/work/adminmag/controller/AdminController.java
+++ src/main/java/com/wys/work/adminmag/controller/AdminController.java
@@ -1,21 +1,67 @@
 package com.wys.work.adminmag.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.wys.work.adminmag.handleservice.IAdminHandleService;
 import com.wys.work.adminmag.queryservice.IAdminQueryService;
+import com.wys.work.beans.AdminBean;
 
 /**
- * @author Administrator
+ * @author yangyong
  * @version 1.0
  * @created 14-����-2018 13:07:12
  */
+@RequestMapping("/adminmag")
+@RestController
 public class AdminController {
 
-
+	@Resource
 	public IAdminHandleService adminHandleServiceImpl;
+	
+	@Resource
 	public IAdminQueryService adminQueryServiceImpl;
 
-	
+	/**
+	 * 新增
+	 * @param admin
+	 */
+	@RequestMapping(value="/{id}",method= {RequestMethod.POST},produces = {"application/json;charset=utf-8"})
+	public void saveAdmin(AdminBean admin) {
+		
+		admin.setId(null);
+		adminHandleServiceImpl.saveAdmin(admin);
+		
+	}
 
+	/**
+	 * 删除
+	 * @param admin
+	 */
+	@RequestMapping(value="/{id}",method= {RequestMethod.DELETE},produces = {"application/json;charset=utf-8"})
+	public void deleteAdmin(AdminBean admin) {
+		
+		adminHandleServiceImpl.deleteAdmin(admin);
+		
+	}
+	
+	/**
+	 * 修改
+	 * @param admin
+	 */
+	@RequestMapping(value="/{id}",method= {RequestMethod.PUT},produces = {"application/json;charset=utf-8"})
+	public void updateAdmin(AdminBean admin) {
+		
+		adminHandleServiceImpl.updateAdmin(admin);
+		
+	}
+	
+	
+	
+	
 	
 
 }
