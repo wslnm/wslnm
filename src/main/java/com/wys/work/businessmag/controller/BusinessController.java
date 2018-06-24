0a5c211a3088b02a520e9e101fc857e3ac43f993
Administrator <Administrator@QH-20151020DXHP>
2018-06-24 15:08:58 +0800

    添加businessmag中controller内容

--- src/main/java/com/wys/work/businessmag/controller/BusinessController.java
+++ src/main/java/com/wys/work/businessmag/controller/BusinessController.java
@@ -1,24 +1,66 @@
 package com.wys.work.businessmag.controller;
 
 import com.wys.work.businessmag.queryservice.IBusinessQueryService;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wys.work.beans.BusinessBean;
 import com.wys.work.businessmag.handleservice.IBusinessHandleService;
 
 /**
- * @author Administrator
+ * @author yangyong
  * @version 1.0
  * @created 14-����-2018 13:07:12
  */
+@RequestMapping("/businessmag")
+@RestController
 public class BusinessController {
+	
+	@Resource
+	public IBusinessQueryService businessQueryServiceImpl;
+	
+	@Resource
+	public IBusinessHandleService businessHandleServiceImpl;
 
-	public IBusinessQueryService m_IBusinessQueryService;
-	public IBusinessHandleService m_IBusinessHandleService;
-
-	public BusinessController(){
-
+	/**
+	 * 新增
+	 * @param business
+	 */
+	@RequestMapping(value="/savebusiness",method= {RequestMethod.POST},produces = {"application/json;charset=utf-8"})
+	public void saveBusiness(BusinessBean business) {
+		
+		businessHandleServiceImpl.saveBusiness(business);
+		
 	}
 
-	public void finalize() throws Throwable {
-
+	/**
+	 * 删除
+	 * @param business
+	 */
+	@RequestMapping(value="/{id}",method= {RequestMethod.DELETE},produces = {"application/json;charset=utf-8"})
+	public void deleteBusiness(BusinessBean business) {
+		
+		businessHandleServiceImpl.deleteBusiness(business);
+		
 	}
+	
+	/**
+	 * 修改
+	 * @param business
+	 */
+	@RequestMapping(value="/{id}",method= {RequestMethod.PUT},produces = {"application/json;charset=utf-8"})
+	public void updateBusiness(BusinessBean business) {
+		
+		businessHandleServiceImpl.updateBusiness(business);
+		
+	}
+	
+	
+	
+	
 
 }
